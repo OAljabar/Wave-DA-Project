31e049b1fe450fad49e9911301e78605b9538ceb
Ivo Pasmans <i.c.pasmans@reading.ac.uk>
2024-07-25 11:26:51 +0100

    Pull from github for correction.

---
+++ ExperimentCode/test.py
@@ -0,0 +1,156 @@
+from models import FugroModel 
+from observations import PointObservation, DictObsReader
+from analyzers import AnalyzerBuilder, ETKF
+from covariances import DiagonalObsCovariance
+from localization import RLocalizer, DistType
+from parallelization import PdafParallelization 
+from inflation import FixedInflator
+from general import *
+import numpy as np
+import pyPDAF.PDAF as PDAF
+from dasystems import DAS
+import os
+
+def create_obs_from_truth(truth,indt,indlt,indlat,indlon,sigoconst,sigoscale,variable):
+    """ 
+    Create observation from artificial truth. 
+    
+    Parameters
+    ----------
+    truth : models.Model object 
+        Truth model after completion of its model run. 
+    indt : list of int 
+        Time indices for which observations are created. 
+    ind0,iind1 : list of int 
+        Observations are made at points (i0,i1) with i0 in 
+        ind0 and i1 in ind1 for every combo of i0,i1.
+    sigoconst : float 
+        Observational error standard deviation constant value
+    sigoscale : float
+        Coefficient for calculating standard deviation as linear function of variable
+    variable : string
+        Name of variable to create observations of
+    
+    Returns
+    -------
+    obs : observations.PointObservation 
+        Observation operator associated with these observations. 
+    """
+    ocoords   = np.array([(it,ilt,ilat,ilon) for it in indt for ilt in indlt
+                          for ilat in indlat
+                          for ilon in indlon])
+    observed  = np.array([truth.fields[variable].data[it,ilt,ilat,ilon] for it,ilt,ilat,ilon in ocoords])
+    std = np.maximum(sigoscale*observed,sigoconst)
+    observed += np.random.normal(scale = std)
+    ocoords = np.array(ocoords, dtype=float)
+    reader = DictObsReader(control.comm_filter,
+                           {'observed': observed,
+                            'coord':ocoords[:,1:],'time':ocoords[:,0]*truth.dt+truth.tinit+0.01,
+                            'variance':std*std})
+
+    obs = PointObservation(control.comm_filter, covariance, reader, truth.nn_interpolator(variable))
+    obs.create_windows_from_model(truth.tinit, nt, truth.dt)
+    print(reader.data)
+    return obs
+
+def calculate_stats(truth, models):
+    """ Calculate statistics of ensemble. """
+    output = np.array([model.saved_output for model in models])
+    ref    = np.array([truth.saved_output for _ in models])
+    output = output.reshape(output.shape[:2] + (-1,))
+    ref    = ref.reshape(ref.shape[:2]+(-1,))
+    print(output.shape)
+    
+    stats = {}
+    stats['spread'] = np.sqrt(np.mean(np.var(output[:,:,:1], axis=0), axis=-1))
+
+    output, ref = np.mean(output,0), np.mean(ref, 0)
+    stats['time'] = truth.save_steps * truth.dt
+    stats['bias'] = np.mean(output - ref, axis=-1)
+    stats['rmse'] = np.sqrt(np.mean((output-ref)**2, axis=-1))
+    stats['correlation'] = np.array([np.corrcoef(o,r)[0,1] for o,r in zip(output,ref)])
+    return stats
+
+#Create process control. Only single process is used here. 
+control = PdafParallelization(1)
+control.initialize()
+#Create truth 
+dt_da = 1.
+tinit = os.environ.get('TINIT')
+if tinit is None:
+    tinit = input('Specify start time:\n')
+tinit = int(tinit)
+tfinal = os.environ.get('TFINAL')
+if tfinal is None:
+    tfinal = input('Specify end time:\n')
+tfinal = int(tfinal)
+nt = int((tfinal - tinit)/dt_da)
+filedir = os.environ['FILEDIR']
+savedir = os.environ['SAVEDIR']
+
+print(filedir)
+print(savedir)
+truth = FugroModel(0,tinit,tfinal,filedir,savedir,label = 'truth',finedomain = True)
+truth.init_fields(control.comm_model)
+
+truth.write_output()
+for _ in range(int(tinit),int(tfinal)):
+    truth.step_forward(truth.step,1)
+    truth.write_output()
+
+nmembers = os.environ.get('NMEMBERS')
+if nmembers is None:
+    nmembers = input('Specify ensemble size:\n')
+nmembers = int(nmembers)
+print('Ensemble size: ',nmembers)
+#Create model for DA. 
+models_da = [FugroModel(member,tinit,tfinal,filedir,savedir,True,label = 'da',finedomain = True)\
+             for member in range(1,nmembers+1)]
+for model in models_da:
+    model.init_fields(control.comm_model)
+    print('Model time: ',model.time)
+#Create reference without da
+models_no = [FugroModel(member,tinit,tfinal,filedir,savedir,True,label = 'no',finedomain = True)\
+             for member in range(1,nmembers+1)]
+for model in models_no:
+    model.init_fields(control.comm_model)
+    model.write_output()
+    for _ in range(int(tinit),int(tfinal)):
+        model.step_forward(model.step,1)
+        model.write_output()
+
+#Create localizer. By default no localization is used. 
+localizer = RLocalizer(pe=control.comm_filter, projection=DistType.cartesian)
+#Covariance. By default errors are assumed to Gaussian. 
+covariance = DiagonalObsCovariance(pe=control.comm_filter, localizer=localizer)
+# no inflation
+inflation_factor = 1
+inflator = FixedInflator(control.comm_filter,1/inflation_factor)
+#Filter. In this case ETKF
+builder = AnalyzerBuilder()
+analyzer = ETKF(pe=control.comm_filter, covariance=covariance, inflator=inflator)
+lat = 18.41364
+lon = -93.7704
+ilat = np.argmin(np.abs(truth.fields.latitude.data - lat))
+ilon = np.argmin(np.abs(truth.fields.longitude.data - lon))
+obswindow = slice(0,3)
+indlt = range(len(truth.fields.leadtime.sel(leadtime = obswindow)))
+#Create wave height observations
+hsobs = create_obs_from_truth(truth,range(nt+1),indlt,[ilat],[ilon],0,0.1,'hs')
+tm01obs = create_obs_from_truth(truth,range(nt+1),indlt,[ilat],[ilon],0.3,0.06,'t01')
+tm02obs = create_obs_from_truth(truth,range(nt+1),indlt,[ilat],[ilon],0.3,0.06,'t02')
+
+#hsobs.write_output(savedir)
+observations = [hsobs,tm01obs,tm02obs]
+#Create and run DA system 
+das = DAS(control, models_da, observations, analyzer, builder)
+das.run(nt+1)
+'''
+#Calculate statics
+stats_no, stats_da = calculate_stats(truth, models_no), calculate_stats(truth, models_da)
+print()
+print('No/DA RMSE', stats_no['rmse'], stats_da['rmse'])
+print('No/DA correlation', stats_no['correlation'], stats_da['correlation'])
+'''
+control.finalize()
+
